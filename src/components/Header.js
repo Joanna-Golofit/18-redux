e252e168b634f7f10c9d7f75067ead8edbf67993
Joanna Gołofit <jgolofit@future-processing.com>
2022-11-18 18:13:40 +0100

    244. Working with Multiple Slices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+// import { useSelector, useDispatch } from 'react-redux';
+// import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
+  // const auth = useSelector(state => state.auth);
+  // const showCounter = useSelector(state => state.);
+
+  // const dispatch = useDispatch();
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
