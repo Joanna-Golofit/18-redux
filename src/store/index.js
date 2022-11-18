247f06a5e0ecd45397523e76e51f8c9c4dfd2816
Joanna Gołofit <jgolofit@future-processing.com>
2022-11-18 16:32:47 +0100

    243. Migrating Everything To Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ const counterSlice = createSlice({
       state.counter++
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     decrement(state) {
       state.counter--
@@ -28,5 +28,7 @@ const counterSlice = createSlice({
 const store = configureStore({
   reducer: counterSlice.reducer
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
